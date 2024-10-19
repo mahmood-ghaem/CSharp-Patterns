ea8a6901052bad31d59ebb19006d28192d463954
mahmood-ghaem <pejvakco@gmail.com>
2024-10-19 14:29:50 +0200

    Add data access

---
+++ src/DataAccess/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<BlogCategory> BlogCategories { get; set; }
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+    }
+}
