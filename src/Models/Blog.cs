bd16d1cd5077072fadebe61828be6494eb1ba3c4
mahmood-ghaem <pejvakco@gmail.com>
2024-10-19 14:29:40 +0200

    Add models

---
+++ src/Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int CategoryId { get; set; }
+        public BlogCategory? Category { get; set; }
+    }
+}
